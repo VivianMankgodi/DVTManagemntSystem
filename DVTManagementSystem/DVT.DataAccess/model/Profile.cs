87213edcb42adfc6afea3f9ed007315aafd59e55
leemuzi123 <leeroyshabangu@yahoo.com>
2017-06-29 10:09:59 +0200

    adding entityframework

---
+++ DVTManagementSystem/DVT.DataAccess/model/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVT.DataAccess.model
+{
+    class Profile
+    {
+        public int ProfileID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+    }
+}

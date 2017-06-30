accbd60ec8f87900f1bb65d905c1c3b52becce96
leemuzi123 <leeroyshabangu@yahoo.com>
2017-06-30 09:08:50 +0200

    changestoprofileclass

--- DVTManagementSystem/DVT.DataAccess/model/Profile.cs
+++ DVTManagementSystem/DVT.DataAccess/model/Profile.cs
@@ -13,10 +13,10 @@ namespace DVT.DataAccess.model
         public int ProfileID { get; set; }
         [Required]
         public string FirstName { get; set; }
-<<<<<<< HEAD
+
 
         public ICollection<Logs> logs { get; set; }
-=======
+
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
@@ -25,6 +25,6 @@ namespace DVT.DataAccess.model
         public virtual ICollection<Department> department { get; set; }
         public virtual ICollection<Gender> gender { get; set; }
         public virtual UserType usertype { get; set; }
->>>>>>> 8f8160e6ea2ad89611e67b8489ff2e359873c11f
+
     }
 }
